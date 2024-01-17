6aa7d099351ae57011e5676b280bfd366d0128f8
tuorqai <tuorqai@gmail.com>
2024-01-17 13:04:16 +0900

    Commit 13

--- src/tyhylge.c
+++ src/tyhylge.c
@@ -3,11 +3,33 @@
  * Icciteq, tuoq da tuhanü ongorbot bibliotieka.
  */
 
+#ifdef TYHYLGE_USE_FREETYPE
+    #include <ft2build.h>
+    #include <freetype/freetype.h>
+#endif
+
 #include "tyhylge.h"
 
 int tyhylge_hello(char const *name)
 {
+#ifdef TYHYLGE_USE_FREETYPE
+    FT_Error ftError;
+    FT_Library ftLibrary;
+    int majVer, minVer, patchVer;
+#endif
+
     printf("Hello from library, %s!\n", name);
+
+#ifdef TYHYLGE_USE_FREETYPE
+    ftError = FT_Init_FreeType(&ftLibrary);
+
+    if (ftError == 0) {
+        FT_Library_Version(ftLibrary, &majVer, &minVer, &patchVer);
+        printf("FreeType version: %d.%d.%d\n", majVer, minVer, patchVer);
+        FT_Done_FreeType(ftLibrary);
+    }
+#endif
+
     return 42;
 }
 
